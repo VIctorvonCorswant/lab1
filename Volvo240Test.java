b0cdf256f1716a68601c8551fa1eeb8484b9acec
Sebastian Brisshäll <sebbri@student.chalmers.se>
2026-01-28 15:03:43 +0100

    Commented and fixed minor things

--- Volvo240Test.java
+++ Volvo240Test.java
@@ -9,40 +9,24 @@ class Volvo240Test {
 
     private Volvo240 volvo;
 
-    /**
-     * Sets up a normal Volvo 240
-     */
+    /** Sets up a normal Volvo 240 */
     @BeforeEach
     public void setUp() {volvo = new Volvo240(Color.green,(double)100);}
 
-
-
-
-    /**
-     * Checks if engine power is correct
-     */
+    /** Checks if engine power is correct */
     @Test
     public void getEnginePower() {
         assertEquals(100, volvo.getEnginePower());
     }
 
-
-
-
-
-
-
-
-
+    /** Checks if speed factor is correct */
     @Test
     public void speedFactor(){
         double expectedSpeedFactor = 100 * 0.01 * 1.25;
         assertEquals(expectedSpeedFactor, volvo.speedFactor(), 0.0001);
     }
 
-    /**
-     * Checks if the gas pedal goes out of bounds
-     */
+    /** Checks if the gas pedal goes out of bounds */
     @Test
     public void throttleOutOfBounds() {
         volvo.engineOn = true;
@@ -53,9 +37,7 @@ class Volvo240Test {
         assertEquals(0, volvo.getCurrentSpeed());
     }
 
-    /**
-     * Checks if the brake pedal goes out of bounds
-     */
+    /** Checks if the brake pedal goes out of bounds */
     @Test
     public void brakeOutOfBounds() {
         volvo.engineOn = true;
@@ -67,12 +49,8 @@ class Volvo240Test {
         assertEquals(40, volvo.getCurrentSpeed());
     }
 
-
-
-        /**
-         * Accelerates and decelerates to check if
-         * speed gets out of intended range
-         */
+    /** Accelerates and decelerates to check if
+     * speed gets out of intended range */
     @Test
     public void carSpeedOutOfRange() {
         volvo.engineOn = true;
