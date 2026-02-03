b13161e86eb00938bc97f956a2fe1675d4a68e78
Mio Falk <mio.falk@icloud.com>
2026-02-03 16:37:20 +0100

    Added test if speed interval exceeds intended range. Also commented all tests. Changes only in Volvo240Test.java

--- Scania.java
+++ Scania.java
@@ -1,10 +1,20 @@
 import java.awt.*;
 
-public /*abstract*/ class Car extends Vehicle {
-    // Initiate variables
+public class Scania implements Movable{
+    /** Initiate variables */
+    Truck truck;
+    private int bedAngle;
+    private boolean turboOn;
+
     public int nrDoors;
     public String modelName;
     public boolean engineOn = false;
+
+    /** Scania constructor */
+    private Scania() {
+        truck = new Truck(Color.blue, 280.0, 2, "Scania");
+
+    }
 
     /** Get the number of doors a car has */
     public int getNrDoors(){
@@ -17,18 +27,18 @@ public /*abstract*/ class Car extends Vehicle {
     }
 
     /** Check if the engine is on */
-    public boolean getEngineOn(){
+    protected boolean getEngineOn(){
         return engineOn;
     }
 
     /** Start the engine */
-    public void startEngine() {if (!this.engineOn) {this.engineOn = true;}}
+    protected void startEngine() {if (!this.engineOn) {this.engineOn = true;}}
 
     /** Stop the engine */
-    public void stopEngine() {if (this.engineOn) {this.engineOn = false;}}
+    protected void stopEngine() {if (this.engineOn) {this.engineOn = false;}}
 
     /** Accelerate by pressing the gas pedal the car */
-    public void gas(double amount){
+    protected void gas(double amount){
         if (amount >= 0 && amount <= 1){
             if(this.getCurrentSpeed() < this.getEnginePower()){
                 incrementSpeed(amount);
@@ -50,6 +60,12 @@ public /*abstract*/ class Car extends Vehicle {
         else {System.out.println("It's either through the floor or  (Brake is out of range.)");}
     }
 
-    /** Does something different for each car type */
-    protected abstract double speedFactor();
+
+    /** turbo get/set methods for saab */
+    public boolean getTurboOn(){return turboOn;}
+
+    public void setTurboOn(){turboOn = true;}
+
+    public void setTurboOff(){turboOn = false;}
+
 }
