abb062cde673152c29e39565afdeaafe9794107d
Sebastian Brisshäll <sebbri@student.chalmers.se>
2026-02-06 14:28:21 +0100

    Added more methods to VolvoFH16 to be able to load/unload cars

--- VolvoFH16.java
+++ VolvoFH16.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class VolvoFH16 extends Truck implements Movable {
 
@@ -6,16 +8,19 @@ public class VolvoFH16 extends Truck implements Movable {
     Truck truck;
     protected boolean trailerUp = true;
     private boolean turboOn;
+    List<Car> trailer = new ArrayList<>();
+    private int trailerSize = 3;
     public boolean engineOn = false;
 
     /** Initialize the constructor */
-    public VolvoFH16(){
-        super(Color.BLACK, 300.0, 2, "Volvo FH16");
+    public VolvoFH16(Color color, double EnginePower, int nrDoors, int trailerSize, String modelName) {
+        super(color, EnginePower, nrDoors, modelName);
+        this.trailerSize = trailerSize;
     }
 
     /** Raise the trailer */
-    private boolean raiseTrailer(boolean raise){
-        if(!trailerUp && truck.getCurrentSpeed() == 0 && raise){
+    public boolean raiseTrailer(boolean raise){
+        if(!trailerUp && getCurrentSpeed() == 0 && raise){
             trailerUp = true;
             return true;
         }
@@ -23,15 +28,33 @@ public class VolvoFH16 extends Truck implements Movable {
     }
 
     /** Lower the trailer */
-    private boolean lowerTrailer(boolean lower) {
-        if (trailerUp && truck.getCurrentSpeed() == 0 && lower) {
+    public boolean lowerTrailer(boolean lower) {
+        if (trailerUp && getCurrentSpeed() == 0 && lower) {
             trailerUp = false;
             return true;
         }
         return false;
     }
 
-    /** turbo get/set methods from saab, because the VolvoFH16 has a turbo */
+    /** Load car to trailer */
+    public boolean loadCar(Car car){
+        if(trailer.size() < trailerSize && getCurrentSpeed() == 0 && !trailerUp){
+            trailer.add(car);
+            return true;
+        }
+        return false;
+    }
+
+     /** Unload car from trailer */
+    public Car unloadCar(){
+        if(!trailer.isEmpty() && getCurrentSpeed() == 0 && !trailerUp){
+            return trailer.remove(trailer.size() - 1);
+            //return trailer.removeLast();
+        }
+        return null;
+    }
+
+    /** turbo from saab, because the VolvoFH16 has a turbo */
     public boolean getTurboOn(){return turboOn;}
 
     public void setTurboOn(){turboOn = true;}
