f6b6b6a35e6790fa5293ec90d6b22bffe56fa85c
Sebastian Brisshäll <sebbri@student.chalmers.se>
2026-02-12 13:28:39 +0100

    Added more test to workshop

--- WorkshopTest.java
+++ WorkshopTest.java
@@ -17,6 +17,11 @@ class WorkshopTest {
     }
 
     @Test
+    void getFacility() {
+        assertNotNull(workshop.getFacility());
+    }
+
+    @Test
     void addCarToWorkshop() {
         Volvo240 car1 = new Volvo240(Color.BLACK, 128.0);
         Volvo240 car2 = new Volvo240(Color.BLUE, 150.0);
@@ -24,5 +29,24 @@ class WorkshopTest {
         workshop.addCarToWorkshop(car2);
         assertTrue(workshop.getFacility().contains(car1));
         assertTrue(workshop.getFacility().contains(car2));
+    }
+
+    @Test
+    void addCarToFullWorkshop() {
+        Volvo240 car1 = new Volvo240(Color.BLACK, 128.0);
+        Volvo240 car2 = new Volvo240(Color.BLUE, 150.0);
+        Volvo240 car3 = new Volvo240(Color.RED, 200.0);
+        workshop.addCarToWorkshop(car1);
+        workshop.addCarToWorkshop(car2);
+        workshop.addCarToWorkshop(car3);
+        assertFalse(workshop.getFacility().contains(car3));
+    }
+
+    @Test
+    void removeCarFromWorkshop() {
+        Volvo240 car1 = new Volvo240(Color.BLACK, 128.0);
+        workshop.addCarToWorkshop(car1);
+        workshop.removeCarFromWorkshop(car1);
+        assertFalse(workshop.getFacility().contains(car1));
     }
 }
